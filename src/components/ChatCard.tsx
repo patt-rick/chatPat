be25a5570fd53bdd04aee95846d6a75014c62c20
minnez <patrickackom003@gmail.com>
2025-03-29 14:03:31 +0000

    chore: ui changes

--- src/components/ChatCard.tsx
+++ src/components/ChatCard.tsx
@@ -9,14 +9,14 @@ const ChatCard = (props: ChatCardProps) => {
     return (
         <Wrapper
             onClick={() => props.onSelect(props.data)}
-            className={`flex gap-2 m-2 cursor-pointer rounded-xl p-2 hover:bg-slate-50 ${
-                props.data.id === props.selectedChatId ? "bg-slate-100" : " "
+            className={`flex gap-2 m-2 cursor-pointer rounded-xl p-2 hover:bg-accent ${
+                props.data.id === props.selectedChatId ? "bg-secondary" : " "
             }`}
         >
             <ProfileImage initial={props.data.clientName[0]} id={props.data.id} />
             <div>
                 <div className="tex-lg">{props.data.clientName}</div>
-                <span className="text-md">{props.data.organisationName}</span>
+                <span className="text-sm">{props.data.organisationName}</span>
             </div>
         </Wrapper>
     );
